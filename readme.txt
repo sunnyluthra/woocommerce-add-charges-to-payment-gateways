612c81beedbb184d491fd2d74da37178647106ea
sunnyluthra <sunny@mrova.com>
2013-04-25 13:21:59 +0530

    First Commit

---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== WooCommerce Add Extra Charges Option To Payment Gateways ===
+Contributors: mRova
+Tags: woocommerce, payment gateway
+Requires at least: 3.5
+Tested up to: 3.5
+Stable tag: 0.1
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Add an option to your WooCommerce payment gateways to add extra charges.
+
+== Description ==
+
+This simple extension allows you to add a extra charges to the payment gateways.
+
+WooCommerce > Settings > Payment Gateways > single gateway page lets you set up an extra charge, which will be added to total whenever user select that particular payment gateway.
+
+If you'd like to contribute to the plugin you can find it on GitHub: [https://github.com/sunnyluthra/woocommerce-add-charges-to-payment-gateways](https://github.com/sunnyluthra/woocommerce-add-charges-to-payment-gateways)
+
+== Installation ==
+
+1. Unzip into your `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. That's it :)
+
+== Screenshots ==
+
+1. The customisable gift message and checkbox
+
+== Changelog ==
+
+= 0.1 - 25/04/2013 =
+* Initial release!
---
+++ woocommerce-add-charges-to-payment-gateways.php
@@ -0,0 +1,133 @@
+<?php
+/*
+Plugin Name: WooCommerce Add Charges To Payment Gateway
+Plugin URI: http://www.mrova.com
+Description: You can add extra charge for any payment gateways
+Version: 0.1
+Author: Sunny Luthra
+Author URI: http://www.mrova.com
+*/
+
+/**
+ * Copyright (c) `date "+%Y"` Sunny Luthra. All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * **********************************************************************
+ */
+class WC_PaymentGateway_Add_Charges{
+    public function __construct(){
+        $this -> current_gateway_title = '';
+        $this -> current_gateway_extra_charges = '';
+        add_action('admin_head', array($this, 'add_form_fields'));
+        add_action( 'woocommerce_calculate_totals', array( $this, 'calculate_totals' ), 10, 1 );
+        add_action('wp_footer', array($this, 'update_checkout'));
+    }
+
+    function add_form_fields(){
+        global $woocommerce;
+         // Get current tab/section
+        $current_tab        = ( empty( $_GET['tab'] ) ) ? '' : sanitize_text_field( urldecode( $_GET['tab'] ) );
+        $current_section    = ( empty( $_REQUEST['section'] ) ) ? '' : sanitize_text_field( urldecode( $_REQUEST['section'] ) );
+        if($current_tab == 'payment_gateways' && $current_section!=''){
+            $gateways = $woocommerce->payment_gateways->payment_gateways();
+            foreach($gateways as $gateway){
+                if(get_class($gateway)==$current_section){
+                    $current_gateway = $gateway -> id;
+                    $extra_charges_id = 'woocommerce_'.$current_gateway.'_extra_charges';
+                    if($_REQUEST[$extra_charges_id]){
+                        update_option( $extra_charges_id, $_REQUEST[$extra_charges_id] );
+                    }
+                    $extra_charges = get_option( $extra_charges_id);
+                }
+            }
+
+            ?>
+            <script>
+            jQuery(document).ready(function($){
+                $data = '<h4>Add Extra Charges</h4><table class="form-table">';
+                $data += '<tr valign="top">';
+                $data += '<th scope="row" class="titledesc">Extra Charges</th>';
+                $data += '<td class="forminp">';
+                $data += '<fieldset>';
+                $data += '<input style="" name="<?php echo $extra_charges_id?>" id="<?php echo $extra_charges_id?>" type="text" value="<?php echo $extra_charges?>"/>';
+                $data += '<br /></fieldset></td></tr></table>';
+                $('.form-table:last').after($data);
+
+            });
+            </script>
+            <?php
+        }
+    }
+
+    public function calculate_totals( $totals ) {
+        global $woocommerce;
+        $available_gateways = $woocommerce->payment_gateways->get_available_payment_gateways();
+        $current_gateway = '';
+        if ( ! empty( $available_gateways ) ) {
+           // Chosen Method
+            if ( isset( $woocommerce->session->chosen_payment_method ) && isset( $available_gateways[ $woocommerce->session->chosen_payment_method ] ) ) {
+                $current_gateway = $available_gateways[ $woocommerce->session->chosen_payment_method ];
+            } elseif ( isset( $available_gateways[ get_option( 'woocommerce_default_gateway' ) ] ) ) {
+                $current_gateway = $available_gateways[ get_option( 'woocommerce_default_gateway' ) ];
+            } else {
+                $current_gateway =  current( $available_gateways );
+
+            }
+        }
+        if($current_gateway!=''){
+            $current_gateway_id = $current_gateway -> id;
+
+            $extra_charges_id = 'woocommerce_'.$current_gateway_id.'_extra_charges';
+            $extra_charges = (int)get_option( $extra_charges_id);
+
+            if($extra_charges){
+                $totals -> cart_contents_total = $totals -> cart_contents_total + $extra_charges;
+                $this -> current_gateway_title = $current_gateway -> title;
+                $this -> current_gateway_extra_charges = $extra_charges;
+                add_action( 'woocommerce_review_order_before_order_total',  array( $this, 'add_payment_gateway_extra_charges_row'));
+
+            }
+
+        }
+        return $totals;
+    }
+
+    function add_payment_gateway_extra_charges_row(){
+        ?>
+        <tr class="payment-extra-charge">
+            <th><?php echo $this->current_gateway_title?> Extra Charges</th>
+            <td><?php echo woocommerce_price($this -> current_gateway_extra_charges)?></td>
+        </tr>
+        <?php
+    }
+
+    function update_checkout(){
+        ?>
+        <script>
+        jQuery(document).ready(function($){
+           $('.payment_methods input.input-radio').live('click', function(){
+            $('#billing_country, #shipping_country, .country_to_state').trigger('change');
+
+           })
+        });
+        </script>
+        <?php
+    }
+
+}
+new Wc_PaymentGateway_Add_Charges();
